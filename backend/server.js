b4d2fd4ca01dc9c61d60ca8b8effa0b37936e170
Tannistha Neogy <83673778+tannisthaneogy@users.noreply.github.com>
2024-08-29 13:41:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yel
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: ["https://chat-app-frontend-qu4k05cs9-tannisthaneogys-projects.vercel.app"],
+        origin: "*",
         methods: ["POST", "GET"],
         credentials: true
     },
